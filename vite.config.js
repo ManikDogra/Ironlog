f57ebf857062c9bbc18406a113594fccbeadafca
InderpreetSingh <inderdevgan90@gmail.com>
2025-11-07 00:55:22 +0530

    Fix: Use direct Elastic Beanstalk URL, remove old EC2 proxy config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://13.202.84.153:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()]
 })
